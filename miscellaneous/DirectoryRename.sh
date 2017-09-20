1574af5b5034efdbe2216baf3831e517eaf47439
robertjirsaraie21@gmail.com <rjirsaraie@chead.uphs.upenn.edu>
2017-09-20 19:23:26 -0400

    IMGcheck_Labels.sh

--- miscellaneous/DirectoryRename.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-
-<<COMMANDLINE
-
-for i in `find /data/joy/BBL/studies/grmpy/rawData/ -iname '*B0map*um*'`; do T=`echo $i | sed 's/_um[0-9][0-9][0-9]/_M/g'`; echo "mv -i $i $T"; done
-
-COMMANDLINE
