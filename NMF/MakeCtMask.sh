9f1364cd02320fa1be1fb6642b7b802b664bce17
robertjirsaraie21@gmail.com <rjirsaraie@compute-0-20.local>
2017-10-23 22:11:53 -0400

    Transforms Dimensions & Creates CT Mask For NMF

--- NMF/MakeCtMask.sh
+++ NMF/MakeCtMask.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 ###################################################################################################
-##########################          GRMPY - NMF Mask Creation            ##########################
+##########################         GRMPY - NMF CT Mask Creation          ##########################
 ##########################              Robert Jirsaraie                 ##########################
 ##########################        rjirsara@pennmedicine.upenn.edu        ##########################
 ##########################                  10/19/2017                   ##########################
@@ -10,8 +10,9 @@
 ###################################################################################################
 <<Use
 
-This script was originally created by Atonia Kaczkurkin and was modifed for the GRMPY dataset. This script
-creates a mask that will remove images with too many 0 or <.1 values using Phil's method.
+This script creates the CT masks needed perform Non-Negative Matrix Factorization (NMF) Analysis.
+The script first increases the dimensions of Raw Images to 2mm, then computes the masks that will
+remove images with too many 0 or <.1 values using Phil's method.
 
 To run this script it is required to have a qlogin session with 50G of memory. Use the Command Below:
 
@@ -27,8 +28,65 @@ Use
 ##############################################################
 
 subjects=$(cat /data/joy/BBL/projects/grmpyProcessing2017/NMF/n115_Cohort_20171015.csv| cut -d ',' -f1)
-outdir=/data/joy/BBL/studies/grmpy/processedData/NMF/ctMask
-mkdir -p ${outdir}
+OutRaw=/data/joy/BBL/studies/grmpy/processedData/NMF/ctRaw
+OutMask=/data/joy/BBL/studies/grmpy/processedData/NMF/ctMask
+mkdir -p ${OutRaw}
+mkdir -p ${OutMask}
+
+########################################################
+### Make Copies of the Raw CT Images To Be Processed ###
+########################################################
+
+i=0
+for s in $subjects; do
+
+   imgList[i]=$(ls /data/joy/BBL/studies/grmpy/processedData/structural/struct_pipeline_20170716/${s}/*/antsCT/*_CorticalThicknessNormalizedToTemplate.nii.gz)
+
+   cp ${imgList[i]} ${OutRaw}
+
+   (( i++ ))
+
+done
+
+###################################################################
+### Applys a Tranformation to Raw Images to Increase Dimensions ###
+###################################################################
+
+copies=/data/joy/BBL/studies/grmpy/processedData/NMF/ctRaw/*CorticalThicknessNormalizedToTemplate.nii.gz
+
+i=0
+for c in $copies; do 
+
+   bblid=$(echo ${c}|cut -d'_' -f1); 
+   datexscanid=$(echo ${c}|cut -d'_' -f2); 
+
+   output[i]=${bblid}_${datexscanid}_CorticalThicknessNormalizedToTemplate_2mm.nii.gz
+
+   antsApplyTransforms -e 3 -d 3 -i ${c} -r /data/joy/BBL/studies/pnc/template/pnc_template_brain_2mm.nii.gz -o ${output[i]}
+
+   (( i++ ))
+
+done
+
+#########################################################
+### Removes the Copied Images if Output is Successful ###
+#########################################################
+
+for o in ${output[@]}; do
+
+   if [[ -f ${o} ]] ; then 
+
+      file=$(echo ${o}| sed s@_2mm@''@g)
+
+      rm $file
+
+      else 
+
+      echo "Output For This Subject Was Not Successful"
+
+   fi
+
+done
 
 ##################################################
 ### Locates the Subjects Image To Be Processed ###
@@ -37,31 +95,32 @@ mkdir -p ${outdir}
 i=0
 for s in $subjects; do
 
-imgList[i]=$(ls /data/joy/BBL/studies/grmpy/processedData/structural/struct_pipeline_20170716/${s}/*/antsCT/*_CorticalThicknessNormalizedToTemplate.nii.gz)
+   List[i]=$(ls /data/joy/BBL/studies/grmpy/processedData/NMF/ctRaw/*${s}*_2mm.nii.gz)
 
-(( i++ ))
+   (( i++ ))
+
 done
 
 #####################################################
 ### Calls the Script that Will Process The Images ###
 #####################################################
 
-for i in ${imgList[@]}; do 
+for i in ${List[@]}; do 
                                                                                                                                                               
-   fileName=$(echo $i | cut -d'/' -f13  | cut -d'.' -f1)
-   echo "file name is $fileName"
+   fileName=$(echo $i | cut -d'/' -f10  | cut -d'.' -f1)
+   echo "file name is ${fileName}"
 
-	ThresholdImage 3 $i ${outdir}/${fileName}_mask.nii.gz 0.1 Inf
-	
+	ThresholdImage 3 $i ${OutMask}/${fileName}_mask.nii.gz 0.1 Inf
+
 done
 
 ########################################################################
 ### Average the Masks Together and Binarize/Threshold the Final Mask ###
 ########################################################################
 
-AverageImages 3 ${outdir}/coverageMask.nii.gz 0 ${outdir}/*mask.nii.gz
+AverageImages 3 ${OutMask}/coverageMask.nii.gz 0 ${OutMask}/*mask.nii.gz
 
-fslmaths ${outdir}/coverageMask.nii.gz -thr .9 -bin ${outdir}/n115_ctMask_thr9_2mm.nii.gz
+fslmaths ${OutMask}/coverageMask.nii.gz -thr .9 -bin ${OutMask}/n115_ctMask_thr9_2mm.nii.gz
 
 ###################################################################################################
 #####  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  ⚡  #####
