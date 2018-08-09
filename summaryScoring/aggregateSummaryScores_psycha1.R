663cb0fea7f67e088e1918c8754a1615dc7e39f4
Kayla Alohilani Piiwaa <32400619+kpiiwaa@users.noreply.github.com>
2018-08-09 12:02:18 -0400

    add demographics script to aggregate script

--- summaryScoring/aggregateSummaryScores_psycha1.R
+++ summaryScoring/aggregateSummaryScores_psycha1.R
@@ -11,5 +11,10 @@ psycha1_allDataSummaryScores <-psycha1_allDataSummaryScores[ which(grumpy$bbl_pr
 source("/data/jux/BBL/projects/grmpyProcessing/grmpyProcessing2017Scripts/summaryScoring/SelfReportScoring.R")
 psycha1_allDataSummaryScores <- merge(scored_StateTrait, psycha1_allDataSummaryScores, "bblid", all.x = TRUE, all.y = TRUE)
 
+source("/data/jux/BBL/projects/grmpyProcessing/grmpyProcessing2017Scripts/summaryScoring/demographics.R")
+psycha1_allDataSummaryScores <- merge(Demo, psycha1_allDataSummaryScores, "bblid", all.x = TRUE, all.y = TRUE)
+
+
+
 ## Create the final CSV ##
 write.csv(psycha1_allDataSummaryScores, paste('/data/jux/BBL/studies/grmpy/rawPsycha1/scored_SelfReport_', currentDate, '.csv', sep = '')
