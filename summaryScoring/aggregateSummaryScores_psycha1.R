91511d06a7fcbca34891768da255a41b7a239270
Kayla Alohilani Piiwaa <32400619+kpiiwaa@users.noreply.github.com>
2018-08-13 15:32:39 -0400

    Calls demographics and self-report scripts
    Will be updated to call other psycha1 scripts once they are completed

--- summaryScoring/aggregateSummaryScores_psycha1.R
+++ summaryScoring/aggregateSummaryScores_psycha1.R
@@ -3,14 +3,21 @@
 #### and combines the results into one csv.              ####
 #############################################################
 
+## Find the correct date to use (aka use the most recent file):
+rawPsycha1 <- list.files("/data/jux/BBL/studies/grmpy/rawPsycha1/")
+rawPsycha1_dates <- regmatches(rawPsycha1, regexpr("[0-9].*[0-9]", rawPsycha1))
+rawPsycha1_dates <- as.numeric(rawPsycha1_dates)
+currentDate <- rawPsycha1_dates[which.max(rawPsycha1_dates)]
+
 ## Create the data-frame to add all summary scores to ##
-psycha1_allDataSummaryScores <- read.csv("/data/jux/BBL/studies/grmpy/rawPsycha1/.R")
+psycha1_allDataSummaryScores <- read.csv(paste("/data/jux/BBL/studies/grmpy/rawPsycha1/demographics_", currentDate, ".csv", sep = ""))
+psycha1_allDataSummaryScores <- data.frame("bblid" = psycha1_allDataSummaryScores$bblid, "famid" = psycha1_allDataSummaryScores$famid)
 psycha1_allDataSummaryScores <-psycha1_allDataSummaryScores[ which(grumpy$bbl_protocol %in% "GRMPY") , ] #removes subjects not listed as GRMPY protcol
 
-##Will need to automate the name using Azeez formatting ##
+# Call the self-report scoring script
 source("/data/jux/BBL/projects/grmpyProcessing/grmpyProcessing2017Scripts/summaryScoring/SelfReportScoring.R")
-psycha1_allDataSummaryScores <- merge(scored_StateTrait, psycha1_allDataSummaryScores, "bblid", all.x = TRUE, all.y = TRUE)
-
+psycha1_allDataSummaryScores <- merge(selfReportScoring, psycha1_allDataSummaryScores, "bblid", all.x = TRUE, all.y = TRUE)
+# Call demographics scoring script
 source("/data/jux/BBL/projects/grmpyProcessing/grmpyProcessing2017Scripts/summaryScoring/demographics.R")
 psycha1_allDataSummaryScores <- merge(Demo, psycha1_allDataSummaryScores, "bblid", all.x = TRUE, all.y = TRUE)
 
